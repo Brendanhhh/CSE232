c29033095905249390a40e13c21eebe4d9ad6bc6
Brendanhhh <155996977+Brendanhhh@users.noreply.github.com>
2024-09-03 11:16:08 -0400

    :3

--- lab-01/new_horizons.cpp
+++ lab-01/new_horizons.cpp
@@ -1,1 +1,21 @@
-// WRITE YOUR CODE HERE
+#include <iostream>
+#include <iomanip>
+
+int main(){
+  double distanceInitAU(37.33);
+  double kMs(14.33);
+  double Ms(8.9);
+  double auToKM(149598000);
+  double auToMi(92955800);
+
+  int days{};
+  std::cin>>days;
+
+  double kmFinal = (distanceInitAU * auToKM) + (86400*kMs*days);
+  double miFinal = (distanceInitAU * auToMi) + (86400*Ms*days);
+  double radioTime = (kmFinal/(299752.458*3600))*2;
+  std::cout << std::fixed << std::setprecision(2);
+  std::cout << kmFinal << std::endl;
+  std::cout << miFinal << std::endl;
+  std::cout << radioTime << std::endl;
+}
